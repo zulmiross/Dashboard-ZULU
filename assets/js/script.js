ab32597c2584016db34000636b4eecddca7af15e
zulmiross <zulejesus@hotmail.com>
2024-06-06 11:26:42 -0300

    uodate arrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,17 @@ jQuery(function($) {
             $(this).parent().addClass("active")
         }
     })
+
+    $(".dropdown").on("click", function() {
+        $(this).toggleClass("active")
+        $(this).hasClass("active") ? $(this).next(".submenu").slideDown(200) : $(this).next(".submenu").slideUp(200)
+    })
+
+    document.documentElement.onclick = function(event) {
+
+        if (event.target == document.querySelector(".nav-link")) {
+            console.log(event.target)
+        }
+    }
+
 })
