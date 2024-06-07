73779919fbf67404f32545aa52111892b7d1a51f
zulmiross <zulejesus@hotmail.com>
2024-06-07 06:49:10 -0300

    update navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,17 @@ jQuery(function($) {
     $(".sidebar-dropdown > a").on('click', function() {
 
         if ($(this).parent().hasClass("active")) {
-            $(this).next(".sidebar-submenu").slideUp(200);
+            $(this).next(".sidebar-submenu").slideUp(100);
             $(this).parent().removeClass("active")
         } else {
-            $(this).next(".sidebar-submenu").slideDown(200);
+            $(this).next(".sidebar-submenu").slideDown(100);
             $(this).parent().addClass("active")
         }
     })
 
     $(".dropdown").on("click", function() {
         $(this).toggleClass("active")
-        $(this).hasClass("active") ? $(this).next(".submenu").slideDown(200) : $(this).next(".submenu").slideUp(200)
+        $(this).hasClass("active") ? $(this).next(".submenu").slideDown(50) : $(this).next(".submenu").slideUp(50)
     })
 
     document.documentElement.onclick = function(event) {
