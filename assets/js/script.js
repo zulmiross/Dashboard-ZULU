b0f26a5d8222ac11acccee0bc36b130b0c503185
zulmiross <zulejesus@hotmail.com>
2024-06-08 18:54:53 -0300

    update sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@ jQuery(function($) {
 
     $(".sidebar-dropdown > a").on('click', function() {
 
+        // closeElement(this)
+
         if ($(this).parent().hasClass("active")) {
             $(this).next(".sidebar-submenu").slideUp(100);
             $(this).parent().removeClass("active")
         } else {
+            $(".sidebar-dropdown").removeClass('active');
+            $(".sidebar-submenu").slideUp(100);
             $(this).next(".sidebar-submenu").slideDown(100);
             $(this).parent().addClass("active")
         }
+
     })
 
     // $(".dropdown").on("click", function() {
@@ -17,8 +22,23 @@ jQuery(function($) {
 
     // })
 
-    document.documentElement.onclick = function(event) {
+    // $(document).on('click', function(e) {
+    //     e.target.closest('.sidebar-dropdown') ? "" : closeElement(e);
+    // });
 
-    }
+    // function closeElement(el) {
+
+    //     if ($(el).parent().hasClass("active")) {
+    //         $(el).next(".sidebar-submenu").slideUp(100);
+    //         $(el).parent().removeClass("active")
+    //     } else {
+    //         $(".sidebar-dropdown").removeClass('active');
+    //         $(".sidebar-submenu").slideUp(100);
+    //         $(el).next(".sidebar-submenu").slideDown(100);
+    //         $(el).parent().addClass("active")
+    //     }
+
+    // }
+
 
 })
